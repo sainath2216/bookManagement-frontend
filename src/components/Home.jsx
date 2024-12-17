0411c7a15821509cefc03fdf7b875654adfaf004
sainath2216 <sainathmusham043@gmail.com>
2024-12-17 13:46:50 +0530

    testing

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
   const booksPerPage = 5; // Number of books per page
 
   useEffect(() => {
-    fetch("http://localhost:5000/api/books")
+    fetch("https://book-management-1szm.onrender.com/api/books")
       .then((response) => response.json())
       .then((data) => setBooks(data))
       .catch((error) => console.error("Error fetching books:", error));
